585a260cb9dd5e01e5a0d1d56f7c7c31bee15ab8
Andrew <andreiduffy@gmail.com>
2017-09-06 13:28:40 +0300

    book links

--- cabinet/index.php
+++ cabinet/index.php
@@ -3,13 +3,12 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Кабинет доверителя");
 ?>
 
-
 <header class="page-header page-header--cabinet">
 	<div class="container">
 		<p class="page-header__title">Кабинет доверителя</p>
 		<p class="page-header__goldtext">Доступ к своему личному делу и документам 24 часа в сутки</p>
 		<p class="page-header__descr">После того, как мы заключим соглашение, Вам будет предоставлен доступ к личному кабинету. Он нужен для того, чтобы Вы могли посмотреть статус своего дела в удобное для Вас время. Также в личном кабинете Вы сможете ознакомиться со всеми документами, подготовленными по Вашему делу.</p>
-		<a href="#" class="btn btn--white">Войти в личный кабинет</a>
+		<a href="http://casepro.pro" class="btn btn--white">Войти в личный кабинет</a>
 		<div class="page-header__cabinet-icons">
 			<div class="page-header__cabinet-icon">
 				<img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-clock.svg" alt="">
@@ -41,14 +40,24 @@ $APPLICATION->SetTitle("Кабинет доверителя");
 		<div class="row-bg">
 
 			<div class="grid-6 grid-7_l grid-12_m">
+				<div class="user-content">
+					<?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => "/includes/spec/ab_text.php"
+					));?>
+				</div>
 				<div class="about-icon-list">
 					<div class="about-icon-list__item">
 						<div class="about-icon-list__item-img">
 							<img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-graph-board.svg" alt="">
 						</div>
 						<div class="user-content">
-							<p>Преподаватель Федерального государственного бюджетного образовательного учреждения «Российский государственный университет правосудия».<br>
-								— Стаж преподавательской работы – 5 лет.</p>
+							<p>
+								<?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/includes/spec/ab_text_1.php"
+								));?>
+							</p>
 						</div>
 					</div>
 					<div class="about-icon-list__item">
@@ -56,9 +65,12 @@ $APPLICATION->SetTitle("Кабинет доверителя");
 							<img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-hat-uni.svg" alt="">
 						</div>
 						<div class="user-content">
-							<p>В течение двух лет работал в Арбитражном суде г. Москвы на разных должностях.<br>
-								— Секретарь судебного заседания.<br>
-								— Помощник председателя судебного состава.</p>
+							<p>
+								<?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/includes/spec/ab_text_2.php"
+								));?>
+							</p>
 						</div>
 					</div>
 					<div class="about-icon-list__item">
@@ -66,7 +78,12 @@ $APPLICATION->SetTitle("Кабинет доверителя");
 							<img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-judge-hammer.svg" alt="">
 						</div>
 						<div class="user-content">
-							<p>Работа в Арбитражном суде г. Москвы была завершена на должности помощника председателя судебного состава, специализирующегося на разрешение споров о собственности.</p>
+							<p>
+								<?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/includes/spec/ab_text_3.php"
+								));?>
+							</p>
 						</div>
 					</div>
 					<div class="about-icon-list__item">
@@ -74,7 +91,12 @@ $APPLICATION->SetTitle("Кабинет доверителя");
 							<img src="<?=SITE_TEMPLATE_PATH?>/img/svg/icons/icon-book-pencil.svg" alt="">
 						</div>
 						<div class="user-content">
-							<p>Автор многочисленных научных статей и статей практического характера для бизнеса.</p>
+							<p>
+								<?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+									"AREA_FILE_SHOW" => "file",
+									"PATH" => "/includes/spec/ab_text_4.php"
+								));?>
+							</p>
 						</div>
 					</div>
 				</div>
