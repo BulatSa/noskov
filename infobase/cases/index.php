805daa95c1df12c88f1cf76b90642e351d87a115
Bulat <sadikovba@gmail.com>
2017-09-26 10:16:23 +0300

    Fix cases count to 8

--- infobase/cases/index.php
+++ infobase/cases/index.php
@@ -68,7 +68,7 @@ $APPLICATION->SetTitle("Завершенные проекты");
 		"MESSAGE_404" => "",
 		"META_DESCRIPTION" => "-",
 		"META_KEYWORDS" => "-",
-		"NEWS_COUNT" => "16",
+		"NEWS_COUNT" => "8",
 		"PAGER_BASE_LINK_ENABLE" => "N",
 		"PAGER_DESC_NUMBERING" => "N",
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
